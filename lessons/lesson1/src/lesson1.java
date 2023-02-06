142ebdd8b98995bf2776c1eb2a1c53db1cff4a3c
sapalamut <babayanf@gmail.com>
2023-02-06 22:03:58 +0400

    new commit

---
+++ lessons/lesson1/src/lesson1.java
@@ -0,0 +1,32 @@
+public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        byte a = 12;
+        System.out.println(a);
+
+        int b = 1233;
+        System.out.println(b);
+
+        short c = 13322;
+        System.out.println(c);
+
+        long d = 3123112L;
+        System.out.println(d);
+
+        float e = 34.1F;
+        System.out.println(e);
+
+        boolean f = true;
+        System.out.println(f);
+
+        double g = 32.4;
+        System.out.println(g);
+
+        char k = '@';
+        System.out.println(k);
+
+        char q = '\uc043';
+        System.out.println(q);
+    }
+}
---
+++ lessons/lesson2/operators/src/Operations.java
@@ -0,0 +1,34 @@
+public class Operations {
+    public static void main(String[] args) {
+        int x = 10, b =7;
+        int c = x + b;
+        System.out.println(c);
+
+        c = x - b;
+        System.out.println(c);
+
+        System.out.println(x * b);
+
+        System.out.println(x/b);
+        System.out.println(x%b);
+
+        int h = 8;
+        h++;
+        System.out.println(h);
+
+
+        x = 40;
+        System.out.println(x % 2 == 0);
+
+        x = 41;
+        System.out.println(x % 2 == 0);
+
+        x = 20;
+        System.out.println(x >=1 && x <=30);
+
+        boolean number = x < 0 || x > 100;
+        System.out.println(number);
+
+    }
+
+}
